09e23d331160b5b9a4a42e97562b29644a8fd3fe
DuEwa Gordon <duewamgordon@gmail.com>
2023-08-18 09:14:08 -0700

    node3

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,20 +5,20 @@
  * @parent: Pointer to the parent node of the node to create
  * @value: Value to put in the new node
  *
- * Return: Pointer to the new node, or NULL on failure
+ * Return: Pointer to the node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
-
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
-
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-
-    return (node);
+	binary_tree_t *node;
+	
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	
+	return (node);
 }
