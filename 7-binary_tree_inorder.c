83e0abb3a4db7785310315b7ea4facceed2ff2d0
DuEwa Gordon <duewamgordon@gmail.com>
2023-08-19 07:05:15 -0700

    binaryinorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,10 +7,13 @@
  *
  * This function traverses the binary tree in in-order and applies the provided
  * function to each node's value.
- * Description: in-order traversal of the binary tree
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
