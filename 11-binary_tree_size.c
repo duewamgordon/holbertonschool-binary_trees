2bb72695aa87d965cdfeced7e5a754e757d191c0
DuEwa Gordon <duewamgordon@gmail.com>
2023-08-21 17:01:25 -0700

    binarysize1

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,15 +3,17 @@
 /**
  * binary_tree_size - Measures the size of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the size.
- * Return: Size of the tree. If @tree is NULL, returns 0.
+ * Return: size_t
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t right_height = 0, left_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
+	right_height = binary_tree_size(tree->right);
+	left_height = binary_tree_size(tree->left);
 
-	return (1 + left_size + right_size);
+	return (1 + right_height + left_height);
 }
