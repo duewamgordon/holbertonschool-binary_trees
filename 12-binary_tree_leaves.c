86297a49be362fee6b68c0223d7c135601623eff
DuEwa Gordon <duewamgordon@gmail.com>
2023-08-21 17:19:07 -0700

    binaryleaves3

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"  
+#include "binary_trees.h"
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree.
@@ -8,7 +8,7 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 
